d7290ae11027ec5c99a2e511ac9a428fd4a14bbe
elifoster <elifosterwy@gmail.com>
2016-04-22 10:15:10 -0700

    Description row is now text instead of varchar. Close #29

--- install/sql/ext_tilesheet_languages.sql
+++ install/sql/ext_tilesheet_languages.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS /*_*/ext_tilesheet_languages (
   `entry_id` int(11) NOT NULL,
   `lang` varchar(10) NOT NULL,
   `display_name` varchar(100),
-  `description` varchar(100)
+  `description` text
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/entry_id ON /*_*/ext_tilesheet_languages(`entry_id`);
