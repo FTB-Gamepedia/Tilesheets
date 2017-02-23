0e93f860b956c20fd6fbfaa7c5bb3ba4fd378351
elifoster <elifosterwy@gmail.com>
2017-02-22 23:47:56 -0800

    Set entry_id and lang as a composite primary key for lang table

--- install/sql/ext_tilesheet_languages.sql
+++ install/sql/ext_tilesheet_languages.sql
@@ -2,7 +2,8 @@ CREATE TABLE IF NOT EXISTS /*_*/ext_tilesheet_languages (
   `entry_id` int(11) NOT NULL,
   `lang` varchar(10) NOT NULL,
   `display_name` varchar(100),
-  `description` text
+  `description` text,
+  PRIMARY KEY (entry_id, lang)
 ) /*$wgDBTableOptions*/;
 
 CREATE INDEX /*i*/entry_id ON /*_*/ext_tilesheet_languages(`entry_id`);
