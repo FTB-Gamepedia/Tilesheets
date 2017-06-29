0b9a3a610fa135ebc6f1210830087494c8db8d57
Alexia E. Smith <washuu@gmail.com>
2017-06-29 16:38:15 -0500

    Missing special page alias file.

---
+++ Tilesheets.alias.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Tilesheets
+ * Tilesheets Aliases
+ *
+ * @license		MIT
+ * @package		Tilesheets
+ * @link		https://github.com/FTB-Gamepedia/Tilesheets
+**/
+
+$specialPageAliases = [];
+
+/** English (English) */
+$specialPageAliases['en'] = [
+	'CreateTileSheet' => ['CreateTileSheet'],
+	'SheetList' => ['SheetList'],
+	'SheetManager' => ['SheetManager'],
+	'TileList' => ['TileList'],
+	'TileManager' => ['TileManager'],
+	'TileTranslator' => ['TileTranslator']
+];
