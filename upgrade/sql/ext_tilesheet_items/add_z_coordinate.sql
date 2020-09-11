02c94d0aa698ff154e72158295fe05fc8048dbda
chaud <chaud@users.noreply.github.com>
2020-09-11 10:53:29 -0500

    Update add_z_coordinate.sql
    Remove comment that was causing problem

--- upgrade/sql/ext_tilesheet_items/add_z_coordinate.sql
+++ upgrade/sql/ext_tilesheet_items/add_z_coordinate.sql
@@ -1,4 +1,3 @@
-/* Populate the column and set NOT NULL after. We don't want a default on this column. */
 ALTER TABLE /*_*/ext_tilesheet_items ADD `z` int(11) NULL;
 UPDATE /*_*/ext_tilesheet_items SET `z` = 0;
 ALTER TABLE /*_*/ext_tilesheet_items MODIFY `z` int(11) NOT NULL;
